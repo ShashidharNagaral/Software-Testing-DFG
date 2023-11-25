e9344b859eb89123cddd9e351d6a8354058fba57
Jayaa Shree Laxmi Kishoore <jayashreelaxmik1998@gmail.com>
2023-11-25 18:20:49 +0530

    created all .java files for 6 different chess pieces

---
+++ Chess Game/src/main/java/Bishop.java
@@ -0,0 +1,10 @@
+public class Bishop extends ChessPiece{
+    public Bishop(int x, int y, COLOR color, ChessBoard chessBoard) {
+        super(x, y, color, chessBoard);
+    }
+
+    // TODO: here we will write the bishop move rules
+    private boolean bishopMovement(int newX, int newY) {
+        return false;
+    }
+}
---
+++ Chess Game/src/main/java/Queen.java
@@ -0,0 +1,10 @@
+public class Queen extends ChessPiece{
+    public Queen(int x, int y, COLOR color, ChessBoard chessBoard) {
+        super(x, y, color, chessBoard);
+    }
+
+    // TODO: here we will write the queen move rules
+    private boolean queenMovement(int newX, int newY){
+        return false;
+    }
+}
